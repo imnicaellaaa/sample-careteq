c01c5fadbf7c6690eb3f14f1235540c9f5a81fb2
imnicaellaaa <nsfrancia18@gmail.com>
2022-06-26 19:06:09 +0800

    updated 6/26/2022

--- app/Http/Controllers/scheduleController.php
+++ app/Http/Controllers/scheduleController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class scheduleController extends Controller
 {
     public function index(){
-        return view("info.schedule");
+        return view("telecare.schedule");
     }
 }
