1593ed1fbbe7d4e1ce2f2aebeea56a19862ed6b0
imnicaellaaa <nsfrancia18@gmail.com>
2022-06-29 09:48:46 +0800

    updated 6/29/2022

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name Admin', 'Hello world') }}</title>
+    <title>{{ config('app.name', 'Hello world') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/layouts/guestadmin.blade.php
+++ resources/views/layouts/guestadmin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name Admin', 'Careteq Admin Panel') }}</title>
+    <title>{{ config('app.name ', 'Careteq Admin Panel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
